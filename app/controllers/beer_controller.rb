a51442b45f500ab09cc1d6e50e9d98f36302344b
Minh Phan <minh.phan@sendgrid.com>
2014-06-26 17:11:58 -0600

    more changes

--- app/controllers/beer_controller.rb
+++ app/controllers/beer_controller.rb
@@ -2,7 +2,28 @@ class BeerController < ApplicationController
 
   def index
     @beerlist = []
+    
+    # TODO Fetch beer list here
+    
+    # FIXME Dummy Data
     @beerlist << {:upc => "123123", :name => "a bier", :qty => 5, :type => "IPA"}
     @beerlist << {:upc => "541354", :name => "another bier", :qty => 21, :type => "IPA"}
+  end
+
+  def edit
+    if request.get?
+        # TODO Fetch beer info
+
+        # FIXME Dummy Data
+        @beer = {:upc => "123123", :name => "a bier", :qty => 5, :type => "IPA"}
+    elsif request.put?
+    elsif request.delete?    
+    end
+  end
+
+  def add
+    if request.get?
+    elsif request.post?
+    end
   end
 end
