bcb0e3b302a1b609a7a3831b41315c7b91408fa8
Minh Phan <minh.phan@sendgrid.com>
2014-06-27 09:25:32 -0600

    added api uri

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module Admin
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.beer = ActiveSupport::OrderedOptions.new 
-    config.beer.endpoint = "http://localhost"
+    config.beer.endpoint = "http://localhost/api"
   end
 end
