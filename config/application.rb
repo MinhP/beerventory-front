b4fe9ddc29eb3dfc1de532419b78e6e43e943b81
Minh Phan <minh.phan@sendgrid.com>
2014-06-27 10:47:25 -0600

    changed endpoint for api

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module Admin
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.beer = ActiveSupport::OrderedOptions.new 
-    config.beer.endpoint = "http://localhost/api"
+    config.beer.endpoint = "http://localhost:9000"
   end
 end
